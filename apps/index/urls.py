ce7b47aa43e8358c5a392dd063861964e2d6f3b4
danielchikara <brandonnakama@gmail.com>
2020-07-19 13:46:37 -0500

    create url list novel

--- apps/index/urls.py
+++ apps/index/urls.py
@@ -11,6 +11,7 @@ path('logout/', views.logout_view, name='logout'),
 path('index/user_novel/create/', views.CreateUserNovel.as_view(), name='user'),
 path('index/user_novel/update/<pk>/', views.UpdateUserNovel.as_view(), name='userupdate'),
 path('index/novel/create/', views.CreateNovel.as_view(), name='novel_create'),
-path('index/user_novel/update/<pk>/', views.UpdateUserNovel.as_view(), name='novel_update'),
+path('index/novel/update/<pk>/', views.UpdateUserNovel.as_view(), name='novel_update'),
+path('index/novel/list/', views.ListNovel.as_view(), name='novel_update'),
 
 ]

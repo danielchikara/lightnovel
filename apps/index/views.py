38abf770ebd726c743b03f05327015b0a066915e
danielchikara <brandonnakama@gmail.com>
2020-07-21 19:20:36 -0500

    queryset

--- apps/index/views.py
+++ apps/index/views.py
@@ -17,7 +17,6 @@ import cv2
 
 
 def resized_image(path, folder):   
-   
     image = imread(path,plugin='matplotlib')
     width = 250
     height = 334
@@ -36,8 +35,7 @@ class IndexView(ListView):
     model = Novel
     context_object_name = 'novel_list'
     
-    #def get_context_data(self, **kwargs):
-        #context['pk_user'] = self.user.pk
+    
 
 class ChapterView(TemplateView):
     template_name = "chapters/view.html"
@@ -69,14 +67,13 @@ class CreateUserNovel(CreateView):
         form.instance.image = image_url
         return super(CreateUserNovel, self).form_valid(form)
 
+
 @method_decorator(login_required(login_url=reverse_lazy('index:login')), name='dispatch')
 class UpdateUserNovel(UpdateView):
     template_name = "user/user.html"
     model = UserNovel
     form_class = UserNovelForm
     success_url = reverse_lazy('index:home')
-    
-    
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -92,8 +89,6 @@ class UpdateUserNovel(UpdateView):
             image_url = self.request.FILES['image']
             image_url = upload_image_file(image_url,'userNovel/')
             form.instance.image = image_url
-        
-        
         return super(UpdateUserNovel,self).form_valid(form)
 
 @method_decorator(login_required(login_url=reverse_lazy('index:login')), name='dispatch')
@@ -102,19 +97,16 @@ class CreateNovel(CreateView):
     model = Novel
     form_class = NovelForm
     
-    
     def get_queryset(self):
         user = self.request.user.id
         queryset = Novel.objects.all()
         queryset = queryset.filter(user_novel__user_profile=user)
         return queryset
 
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['operation'] = 'Crear.'
         context['title1'] = 'Creacion de Novela.'
-        
         return context
 
     def form_valid(self, form):
@@ -127,6 +119,7 @@ class CreateNovel(CreateView):
     def get_success_url(self):
         pk = self.object.pk
         return reverse('index:chapter_create', kwargs={'pk': pk})
+
 
 @method_decorator(login_required(login_url=reverse_lazy('index:login')), name='dispatch')
 class UpdateNovel(UpdateView):
@@ -141,7 +134,6 @@ class UpdateNovel(UpdateView):
         queryset = queryset.filter(user_novel__user_profile=user)
         return queryset
    
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         instance = self.get_object()
@@ -158,6 +150,7 @@ class UpdateNovel(UpdateView):
             form.instance.image = image_url
         return super(UpdateNovel,self).form_valid(form)
 
+
 @method_decorator(login_required(login_url=reverse_lazy('index:login')), name='dispatch')
 class ListNovel(ListView):
     template_name = 'novels/list.html'
@@ -173,9 +166,6 @@ class ListNovel(ListView):
         return queryset
 
     
-    
-
-
 @method_decorator(login_required(login_url=reverse_lazy('index:login')), name='dispatch')
 class CreateChapter(CreateView):
     template_name = "chapters/register.html"
@@ -183,9 +173,12 @@ class CreateChapter(CreateView):
     form_class = ChapterForm
     success_url = reverse_lazy('index:home')
 
-    
-
-     
+    def get_queryset(self):
+        user = self.request.user.id
+        queryset = Chapter.objects.all()
+        queryset = queryset.filter(novel__user_novel__user_profile=user)
+        return queryset
+  
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['operation'] = 'Crear.'
@@ -200,14 +193,19 @@ class CreateChapter(CreateView):
         form.instance.image = image_url            
         return super(CreateChapter, self).form_valid(form)
 
+
 @method_decorator(login_required(login_url=reverse_lazy('index:login')), name='dispatch')
 class UpdateChapter(UpdateView):
     template_name = "chapters/register.html"
     model = Chapter
     form_class = ChapterForm
     success_url = reverse_lazy('index:home')
-
     
+    def get_queryset(self):
+        user = self.request.user.id
+        queryset = Chapter.objects.all()
+        queryset = queryset.filter(novel__user_novel__user_profile=user)
+        return queryset
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -224,22 +222,20 @@ class UpdateChapter(UpdateView):
             form.instance.image = image_url
         return super(UpdateChapter,self).form_valid(form)
 
+
 @method_decorator(login_required(login_url=reverse_lazy('index:login')), name='dispatch')
 class ListChapter(ListView):
     template_name = 'chapters/list.html'
     model = Chapter 
     context_object_name = 'chapter_list'
 
+    def get_queryset(self):
+        user = self.request.user.id
+        queryset = Chapter.objects.all()
+        queryset = queryset.filter(novel__user_novel__user_profile=user)
+        return queryset
+
     
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        pk_ = get_object_or_404(Novel, pk=self.kwargs['pk'])
-        context['title'] = pk_
-        context['list'] = Chapter.objects.all().filter(novel=pk_)
-        return context
-
-
 
 class LoginView(TemplateView):
     template_name = "Login/login.html"
@@ -267,6 +263,7 @@ class LoginView(TemplateView):
             except:
                 return redirect('index:user')
         return render(request, self.template_name, {'form': form })
+
 
 def logout_view(request):
     if request.user.is_authenticated:

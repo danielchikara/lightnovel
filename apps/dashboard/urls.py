7116079d2b15ac6be931204c5dd98be49bcece20
danielchikara <brandonnakama@gmail.com>
2020-07-25 16:45:49 -0500

    fix create user

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 
 path('', views.DashboardView.as_view(), name='home'),
 path('user_gestion/', views.UserGestionView.as_view(), name='user_gestion'),
-path('user_gestion/create', views.CreateRegister.as_view(), name='create_register'),
+path('user_gestion/create/', views.CreateRegister.as_view(), name='create_register'),
 path('user_gestion/update<pk>/', views.UpdateRegister.as_view(), name='update_register'),
 ]
